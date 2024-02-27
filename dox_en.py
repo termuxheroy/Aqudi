fdfecb2e807cb471c0ae8689120a5befddf5413c
termuxheroy <144564892+termuxheroy@users.noreply.github.com>
2024-02-27 17:44:09 +0300

    Update dox_en.py
    123

--- dox_en.py
+++ dox_en.py
@@ -682,33 +682,9 @@ def numero():
             print(f'{Colores.red}[~] Returning to the main menu...')
             time.sleep(1)
             numero()
-    elif var == 4:
-	    def lookup(phone_number):
-		    http = requests.get(f"https://free-lookup.net/{phone_number}")
-		    html = htmlparser(http.text, "html.parser")
-		    infos = html.findChild("ul", {"class": "report-summary__list"}).findAll("div")
-
-    return {k.text.strip(): infos[i+1].text.strip() if infos[i+1].text.strip() else "No information" for i, k in enumerate(infos) if not i % 2}
-
-def main():
-    while True:
-        try:
-            phone_number = input("Phone number: ").strip().replace("-", "").replace(" ", "").replace("+", "")
-        except KeyboardInterrupt:
-            return
-
-        try:
-            infos = lookup(phone_number)
-        except AttributeError:
-            print("Error: Invalid phone number\n")
-            continue
-
-        [print(f"{info}: {infos[info]}") for info in infos]
-        print("\n")
-
-if __name__ == "__main__":
-    main()
-	    
+    elif var == 4:	    
+	
+	  
 def osintpa():
   os.system("clear")
   print(logo)
